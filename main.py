0dc242fe16a4e063c958939bdad93e82474b69be
PD-Mera <phuongdong1772000@gmail.com>
2024-01-24 03:04:39 +0000

    fix typo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PIL import Image
-from pyblur import RandomizedBlur, DEFAULT_BLUR_FUNCTION
+from pyblur import RandomizedBlur, DEFAULT_BLUR_FUNCTIONS
 
-blurFunctions = DEFAULT_BLUR_FUNCTION.copy()
+blurFunctions = DEFAULT_BLUR_FUNCTIONS.copy()
 blurFunctions["StochasticMotionBlur"]["intensity_min"] = 0.2
 
 img = Image.open("assets/test256.png").convert("RGB")
